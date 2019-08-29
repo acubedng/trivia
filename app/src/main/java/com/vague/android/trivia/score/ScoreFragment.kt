9ec92bd09775725c75ef04cf7734859e772f0e67
Vague <vaguemail369@gmail.com>
2019-08-29 19:14:25 +0100

    Create a ViewModel for ScoreFragment

--- app/src/main/java/com/vague/android/trivia/score/ScoreFragment.kt
+++ app/src/main/java/com/vague/android/trivia/score/ScoreFragment.kt
@@ -1,7 +1,27 @@
 package com.vague.android.trivia.score
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.vague.android.trivia.R
+import com.vague.android.trivia.databinding.FragmentScoreBinding
 
 class ScoreFragment : Fragment() {
 
+    private lateinit var binding: FragmentScoreBinding
+    private lateinit var viewModel: ScoreViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_score, container, false)
+
+        val scoreArgs: ScoreFragmentArgs by navArgs()
+
+        viewModel = ViewModelProvider(this, ScoreViewModelFactory(scoreArgs.score)).get(ScoreViewModel::class.java)
+        return binding.root
+    }
 }
---
+++ app/src/main/java/com/vague/android/trivia/score/ScoreViewModel.kt
@@ -0,0 +1,5 @@
+package com.vague.android.trivia.score
+
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel(score: Int) : ViewModel()
