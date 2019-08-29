b908ad887804a2d189f26811e23177b6541c3293
Vague <vaguemail369@gmail.com>
2019-08-29 12:52:20 +0100

    Replace UI inflation with DataBinding in TitleFragment

--- app/src/main/java/com/vague/android/trivia/title/TitleFragment.kt
+++ app/src/main/java/com/vague/android/trivia/title/TitleFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.vague.android.trivia.R
+import com.vague.android.trivia.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
+    private lateinit var binding: FragmentTitleBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_title, container, false)
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        return binding.root
     }
 }
