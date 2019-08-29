9284d21bc4c730a1a5438554b939bedf08c425be
Vague <vaguemail369@gmail.com>
2019-08-29 13:51:40 +0100

    Setup DataBinding in GameFragment

--- app/src/main/java/com/vague/android/trivia/game/GameFragment.kt
+++ app/src/main/java/com/vague/android/trivia/game/GameFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.vague.android.trivia.R
+import com.vague.android.trivia.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
+    lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        return binding.root
     }
 }
