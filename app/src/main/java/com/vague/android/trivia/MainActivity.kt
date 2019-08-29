f09e628ef7ea6ab1d7cec3ec6e7c8bbc05e1b5a1
Vague <vaguemail369@gmail.com>
2019-08-29 04:19:14 +0100

    Initial Commit

---
+++ app/src/main/java/com/vague/android/trivia/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.vague.android.trivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
