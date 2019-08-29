7db8377f95dd61eeb18821589b0803c60635d50d
Vague <vaguemail369@gmail.com>
2019-08-29 14:05:47 +0100

    Create a ViewModel for GameFragment

--- app/src/main/java/com/vague/android/trivia/game/GameFragment.kt
+++ app/src/main/java/com/vague/android/trivia/game/GameFragment.kt
@@ -6,15 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.vague.android.trivia.R
 import com.vague.android.trivia.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
 
     lateinit var binding: FragmentGameBinding
+    lateinit var viewModel: GameViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         return binding.root
     }
---
+++ app/src/main/java/com/vague/android/trivia/game/GameViewModel.kt
@@ -0,0 +1,15 @@
+package com.vague.android.trivia.game
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    init {
+        //TODO: Start a countdown timer
+    }
+
+    companion object {
+        const val SECOND = 1000
+        const val QUESTION_TIME = 3000
+    }
+}
