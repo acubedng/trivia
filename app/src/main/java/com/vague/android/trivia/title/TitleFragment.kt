16db8f72ff3f28dceb24682a6444db7ed9be322a
Vague <vaguemail369@gmail.com>
2019-08-29 12:59:25 +0100

    Allow navigation to GameFragment

--- app/src/main/java/com/vague/android/trivia/title/TitleFragment.kt
+++ app/src/main/java/com/vague/android/trivia/title/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.vague.android.trivia.R
 import com.vague.android.trivia.databinding.FragmentTitleBinding
 
@@ -16,6 +17,10 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        binding.titleBtnStartgame.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
 
         return binding.root
     }
